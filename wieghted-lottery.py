c5f959a33929241fafc33c1f64e09b80b117ab72
Phil Hawker <philhawker1211@gmail.com>
2020-01-20 14:20:12 -0700

    uploaded simple weighted lottery program

--- wieghted-lottery.py
+++ wieghted-lottery.py
@@ -1,18 +1,19 @@
 import numpy as np
 
 def weighted_lottery(weights):
-  container_list = []
+    container_list = []
+    for (name, weight) in weights.items():
+        for _ in range(weight):
+            container_list.append(name)
 
-  for (name, weight) in weights.items():
-    for _ in range(weight):
-      container_list.append(name)
-      
-  return container_list
+    return np.random.choice(container_list)
+
 
 other_weights = {
-  'winning': 1,
-  'break_even': 2,
-  'losing': 3,
-  }
+    'winning': 1,
+    'break_even': 2,
+    'losing': 3,
+}
+
 
 print(weighted_lottery(other_weights))
