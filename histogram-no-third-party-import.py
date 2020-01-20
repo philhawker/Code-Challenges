f0851703bf0fbca71e9719227e034e25160d9e80
Phil Hawker <philhawker1211@gmail.com>
2020-01-20 15:30:48 -0700

    uploaded manual histogram program

---
+++ histogram-no-third-party-import.py
@@ -0,0 +1,11 @@
+sales = {
+  'google': 20,
+  'facebook': 42,
+  'twitter': 10,
+  'offline': 12,
+}
+
+print('g ' +sales['google'] * '$')
+print('f ' +sales['facebook'] * '$')
+print('t ' +sales['twitter'] * '$')
+print('o ' +sales['offline'] * '$')
