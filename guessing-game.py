fec7bf89e7f8e1c513dd7236063e345c47e28210
Phil Hawker <philhawker1211@gmail.com>
2020-01-21 13:15:49 -0700

    added simple guessing game

---
+++ guessing-game.py
@@ -0,0 +1,13 @@
+# def guessing_game():
+#   while True: 
+#     print('what is your guess?')
+#     guess = input('42')
+    
+#     if guess == '42':
+#       print('your guess is correct')
+#       return False
+#     else:
+#       print(f'no, {guess} is not the answer, please try again')
+      
+
+# guessing_game('42')
