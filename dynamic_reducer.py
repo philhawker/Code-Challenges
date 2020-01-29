3678660fd1f70146ed4b0e1d840cbbe3a866003e
Phil Hawker <philhawker1211@gmail.com>
2020-01-29 10:18:43 -0700

    changed variable naming convention

--- dynamic_reducer.py
+++ dynamic_reducer.py
@@ -10,7 +10,7 @@ import itertools
 num_list = [1, 2, 3]
 
 
-def flexible_counter(collection, op):
+def dynamic_reducer(collection, op):
     operators = {
         "+": operator.add,
         "-": operator.sub,
@@ -21,7 +21,7 @@ def flexible_counter(collection, op):
     return reduce((lambda total, element: operators[op](total, element)), collection)
 
 
-total = flexible_counter([1, 2, 3], '/')  # 0.16666666666666666
+total = dynamic_reducer(num_list, '/') 
 
 print(total)
 
